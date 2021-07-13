c45aa3f8a710ec41b763d0294d61e1d8470a88b9
Suleiman Aliev <suleiman.aliev@jetruby.com>
2021-07-13 21:29:42 +0300

    added procfile and server run

--- bot.js
+++ bot.js
@@ -65,129 +65,16 @@ bot.onText(/(\/show_month)|(\/show_year)/g, async (ctx, match) => {
   const memes = await Mem.find(query, [], { sort:{ rate: -1 } })
   const bestMemes = memes.slice(0, 5)
 
-  const mock = [
-    {
-      _id: '60eccd747a3601e42cc033b4',
-      id: '91',
-      author: 'karmaKiller3352',
-      publicationYear: '2021',
-      publicationMonth: 'июль 2021',
-      title: 'MEM',
-      description: '',
-      videoId: null,
-      photoId: null,
-      pollId: '5431775285903098268',
-      rate: '5',
-      __v: 0
-    },
-    {
-      _id: '60eccdb089d0f7e483a685aa',
-      id: '96',
-      author: 'denis',
-      publicationYear: '2021',
-      publicationMonth: 'июль 2021',
-      title: 'MEM 2',
-      description: '',
-      videoId: null,
-      photoId: null,
-      pollId: '5431775285903098269',
-      rate: '3',
-      __v: 0
-    },
-    {
-      _id: '60eccd747a3601e42cc033b4',
-      id: '91',
-      author: 'denis',
-      publicationYear: '2021',
-      publicationMonth: 'июль 2021',
-      title: 'MEM',
-      description: '',
-      videoId: null,
-      photoId: null,
-      pollId: '5431775285903098268',
-      rate: '11',
-      __v: 0
-    },
-    {
-      _id: '60eccdb089d0f7e483a685aa',
-      id: '96',
-      author: 'maxim',
-      publicationYear: '2021',
-      publicationMonth: 'июль 2021',
-      title: 'MEM 2',
-      description: '',
-      videoId: null,
-      photoId: null,
-      pollId: '5431775285903098269',
-      rate: '5',
-      __v: 0
-    },
-    {
-      _id: 'maxim',
-      id: '91',
-      author: 'karmaKiller3352',
-      publicationYear: '2021',
-      publicationMonth: 'июль 2021',
-      title: 'MEM',
-      description: '',
-      videoId: null,
-      photoId: null,
-      pollId: '5431775285903098268',
-      rate: '2',
-      __v: 0
-    },
-    {
-      _id: '60eccdb089d0f7e483a685aa',
-      id: '96',
-      author: 'alex',
-      publicationYear: '2021',
-      publicationMonth: 'июль 2021',
-      title: 'MEM 2',
-      description: '',
-      videoId: null,
-      photoId: null,
-      pollId: '5431775285903098269',
-      rate: '11',
-      __v: 0
-    },
-    {
-      _id: '60eccd747a3601e42cc033b4',
-      id: '91',
-      author: 'alex',
-      publicationYear: '2021',
-      publicationMonth: 'июль 2021',
-      title: 'MEM',
-      description: '',
-      videoId: null,
-      photoId: null,
-      pollId: '5431775285903098268',
-      rate: '3',
-      __v: 0
-    },
-    {
-      _id: '60eccdb089d0f7e483a685aa',
-      id: '96',
-      author: 'dima',
-      publicationYear: '2021',
-      publicationMonth: 'июль 2021',
-      title: 'MEM 2',
-      description: '',
-      videoId: null,
-      photoId: null,
-      pollId: '5431775285903098269',
-      rate: '12',
-      __v: 0
-    }
-  ]
   const authors = groupByAuthors(memes)
 
   const message = buildMessage(command, authors, bestMemes, chatId)
-  // с <a href="https://t.me/c/${chatId}/${winner.bestMem.id}">ЭТИМ МЕМЧИКОМ</a>
   bot.sendMessage(ctx.chat.id, message, { parse_mode: 'HTML' })
 })
 
-app.listen(3000, function () {
-  console.log('Bot API was runned on port 3000!');
+const port = process.env.PORT || 8080
+connectDb()
+app.listen(port, function () {
+  console.log(`Bot API was runned on port ${port}!`);
 });
 
-connectDb()
+
